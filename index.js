2ef1d0856beba54b704f7ba813a8b2a62a601287
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-12-10 00:04:53 +0500

    update

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const welcomeMessage = document.getElementById("welcomeMessage");
 if (currentUser) {
   welcomeMessage.innerText = `Welcome, ${currentUser}!`;
 } else {
-  alert("Mister please login first...");
+  alert("Sir, please create a accound...");
   window.location.href = "login.html";
 }
 function logout() {
     localStorage.removeItem("currentUser");
     alert("User logged out successfully");
-    window.location.href = "login.html";
+    window.location.href = "signup.html";
   }
   
